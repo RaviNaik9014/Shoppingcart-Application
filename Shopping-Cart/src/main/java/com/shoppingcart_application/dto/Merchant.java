7d26e0053994303350765ecd1ab5f629b596906e
krishna <krishna@RAVI>
2024-05-04 15:45:31 +0530

    class commit

---
+++ Shopping-Cart/src/main/java/com/shoppingcart_application/dto/Merchant.java
@@ -0,0 +1,84 @@
+package com.shoppingcart_application.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Merchant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id ;
+	private String  name ;
+	@Column(unique = true)
+	private long mobilenumber ;
+	@Column(unique = true)
+	private String email ;
+	private String  password ;
+	private String confirmPassword ;
+	
+	@OneToMany
+	private List<Product> product ;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getMobilenumber() {
+		return mobilenumber;
+	}
+
+	public void setMobilenumber(long mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public List<Product> getProduct() {
+		return product;
+	}
+
+	public void setProduct(List<Product> product) {
+		this.product = product;
+	}
+	}	
